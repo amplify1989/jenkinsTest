fb6eff0cd1d4fe6274d912bed9fc0fa54aac5423
Eugenio <eugenio@MacBook-Pro-Eugenio.local>
2022-11-09 21:52:37 +0300

    controller with 1 get method /get

---
+++ src/main/java/ru/sbrf/efs/kubertest/KuberTestApplication.java
@@ -0,0 +1,13 @@
+package ru.sbrf.efs.kubertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuberTestApplication.class, args);
+    }
+
+}
